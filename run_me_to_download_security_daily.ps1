eb60230d857b9edc6932b7ada8abe2b84fa07c7b
huxiaoou <516984451@qq.com>
2022-03-15 17:06:07 +0800

    Delete ps1 files from project

--- run_me_to_download_security_daily.ps1
+++
@@ -1,3 +0,0 @@
-$trade_date = Get-Date -Format "yyyyMMdd"
-python 3_download_security_id_md_fm.py $trade_date htzq12157 247260 T >> .\log\download_security_$trade_date.log
-python 8_copy_to_portal.py $trade_date >> .\log\download_security_$trade_date.log
